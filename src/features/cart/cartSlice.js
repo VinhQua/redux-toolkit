32d667ee35fe8799c1ebd74f59517d37595e95b2
VinhQua <dragonlinuxnoinochi@gmai.com>
2023-07-18 00:07:15 +0900

    useSelector

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+import cartItems from "../../cartItems";
+const initialState = {
+  cartItems: cartItems,
+  amount: 5,
+  total: 4,
+  isLoading: true,
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+});
+
+// console.log(cartSlice);
+
+export default cartSlice.reducer;
--- src/icons.jsx
+++ src/icons.jsx
@@ -1,33 +1,50 @@
 export const CartIcon = () => {
   return (
     <svg
-      xmlns='http://www.w3.org/2000/svg'
-      className='h-6 w-6 '
-      fill='none'
-      viewBox='0 0 24 24'
-      stroke='currentColor'
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6 "
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
       strokeWidth={2}
     >
       <path
-        strokeLinecap='round'
-        strokeLinejoin='round'
-        d='M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z'
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
       />
     </svg>
   );
 };
-
+export const Testing = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 4.5v15m0 0l6.75-6.75M12 19.5l-6.75-6.75"
+      />
+    </svg>
+  );
+};
 export const ChevronDown = () => {
   return (
     <svg
-      xmlns='http://www.w3.org/2000/svg'
-      className='h-6 w-6'
-      fill='none'
-      viewBox='0 0 24 24'
-      stroke='currentColor'
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
       strokeWidth={2}
     >
-      <path strokeLinecap='round' strokeLinejoin='round' d='M19 9l-7 7-7-7' />
+      <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
     </svg>
   );
 };
@@ -35,14 +52,14 @@ export const ChevronDown = () => {
 export const ChevronUp = () => {
   return (
     <svg
-      xmlns='http://www.w3.org/2000/svg'
-      className='h-6 w-6'
-      fill='none'
-      viewBox='0 0 24 24'
-      stroke='currentColor'
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
       strokeWidth={2}
     >
-      <path strokeLinecap='round' strokeLinejoin='round' d='M5 15l7-7 7 7' />
+      <path strokeLinecap="round" strokeLinejoin="round" d="M5 15l7-7 7 7" />
     </svg>
   );
 };
