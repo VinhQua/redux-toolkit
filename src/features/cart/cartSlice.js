91a5f26a7f7e1c33a02fbf048b3f08b13d0dd191
VinhQua <dragonlinuxnoinochi@gmai.com>
2023-07-20 10:28:38 +0900

    calculate items

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -14,11 +14,34 @@ const cartSlice = createSlice({
     clearCart: (state) => {
       state.cartItems = [];
     },
+    removeItem: (state, { payload: id }) => {
+      // console.log(action);
+      state.cartItems = state.cartItems.filter((item) => item.id !== id);
+    },
+    toggle: (state, { payload: { id, option } }) => {
+      const cartItem = state.cartItems.find((item) => item.id === id);
+      if (option === "increase") {
+        cartItem.amount += 1;
+      } else if (option === "decrease") {
+        cartItem.amount -= 1;
+      }
+    },
+    calculateTotals: (state) => {
+      let amount = 0;
+      let total = 0;
+      state.cartItems.forEach((item) => {
+        amount += item.amount;
+        total += item.amount * item.price;
+      });
+      state.amount = amount;
+      state.total = total;
+    },
   },
 });
 
 // console.log(cartSlice);
 
-export const { clearCart } = cartSlice.actions;
+export const { clearCart, removeItem, toggle, calculateTotals } =
+  cartSlice.actions;
 
 export default cartSlice.reducer;
